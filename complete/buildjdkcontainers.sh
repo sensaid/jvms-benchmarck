e4f3703828fe19afab55065d8ded0e9131d93170
said senhadji <elsenhadji@gmail.com>
2019-12-15 23:26:26 -0500

    initial commit 1

--- complete/buildjdkcontainers.sh
+++ complete/buildjdkcontainers.sh
@@ -1,7 +1,7 @@
 #mvn clean package
 
 docker build -t vehicle-information-utility-openjdk -f Dockerfile.openjdk --build-arg JAR_FILE=target/gs-reactive-rest-service-0.1.0.jar .
-#docker build -t vehicle-information-utility-oraclejdk -f Dockerfile.oraclejdk --build-arg JAR_FILE=target/gs-reactive-rest-service-0.1.0.jar .
+docker build -t vehicle-information-utility-oraclejdk -f Dockerfile.oraclejdk --build-arg JAR_FILE=target/gs-reactive-rest-service-0.1.0.jar .
 docker build -t vehicle-information-utility-correttojdk -f Dockerfile.correttojdk --build-arg JAR_FILE=target/gs-reactive-rest-service-0.1.0.jar .
 docker build -t vehicle-information-utility-graalvmce -f Dockerfile.graalvmce --build-arg JAR_FILE=target/gs-reactive-rest-service-0.1.0.jar .
 docker build -t vehicle-information-utility-zulu -f Dockerfile.zulu --build-arg JAR_FILE=target/gs-reactive-rest-service-0.1.0.jar .
